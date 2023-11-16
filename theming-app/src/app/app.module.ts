4bb45169c7b1e21cdf9c8dc71a3217932c5b0a79
Jean Paulo Talar <jeanttalar@gmail.com>
2023-11-16 20:12:26 -0300

    feat: included banner component

--- theming-app/src/app/app.module.ts
+++ theming-app/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BannerComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
