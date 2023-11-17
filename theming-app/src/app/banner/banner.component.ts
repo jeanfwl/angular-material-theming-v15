f3729f996b671c0d543a44158d4de77215f11724
Jean Paulo Talar <jeanttalar@gmail.com>
2023-11-16 21:10:26 -0300

    feat: included banner custom theme color

--- theming-app/src/app/banner/banner.component.ts
+++ theming-app/src/app/banner/banner.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
+  host: { class: 'app-banner' },
 })
-export class BannerComponent {
-
-}
+export class BannerComponent {}
