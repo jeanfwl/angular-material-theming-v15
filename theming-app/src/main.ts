18cea3c6f5cc6db94c5e426353ab75bda4d6a40f
Jean Paulo Talar <jeanttalar@gmail.com>
2023-11-27 21:47:45 -0300

    removed sanity check inside angular material theming

--- theming-app/src/main.ts
+++ theming-app/src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { PLATFORM_INITIALIZER, inject } from '@angular/core';
 import { ThemeManagerService } from './app/core/theme-manager.service';
+import { MATERIAL_SANITY_CHECKS } from '@angular/material/core';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule, {
@@ -10,6 +11,12 @@ platformBrowserDynamic()
       {
         provide: PLATFORM_INITIALIZER,
         useFactory: () => inject(ThemeManagerService).theme$,
+      },
+      {
+        provide: MATERIAL_SANITY_CHECKS,
+        useValue: {
+          theme: false,
+        },
       },
     ],
   })
